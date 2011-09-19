7f144c553fc44c5e4e8c00ef0922aae3f962f2fc
rofl0r <retnyg@gmx.net>
2011-09-19 07:36:26 +0200

    busybox wget fix

--- butch.c
+++ butch.c
@@ -1,3 +1,21 @@
+/*
+    Copyright (C) 2011  rofl0r
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
@@ -356,10 +374,10 @@ int create_script(int ptype, pkgstate* state, pkg* item) {
 		if(!hastarball) abort(); //bug
 		temp = stringptr_concat(SPL("#!/bin/sh\n"),
 			config,
-			SPL("wget -c "),
-			stringptrlist_get(item->data.mirrors, rand() % stringptrlist_getsize(item->data.mirrors)),
-			SPL(" -O $C/"),
+			SPL("wget -O $C/"),
 			stringptr_fromchar(buf, &tb),
+			SPL(" -c "),
+			stringptrlist_get(item->data.mirrors, rand() % stringptrlist_getsize(item->data.mirrors)),
 			NULL);
 		
 	} else if (ptype == 1) {
