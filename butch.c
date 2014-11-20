48464719fd4cb90e0ff1b7ad8becb5202f305bf0
rofl0r <retnyg@gmx.net>
2014-11-20 15:14:21 +0100

    fix copyright years

--- butch.c
+++ butch.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2011,2012  rofl0r
+    Copyright (C) 2011-2014  rofl0r
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
