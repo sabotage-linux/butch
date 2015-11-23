04bc9db0572f451663564d62d61f709215d5a274
rofl0r <retnyg@gmx.net>
2015-11-23 16:07:49 +0000

    create-dist.sh: update

--- create-dist.sh
+++ create-dist.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-if [[ -z "$VER" ]] ; then
+if [ -z "$VER" ] ; then
 	echo set VER!
 	exit
 fi
 
 librepo=http://github.com/rofl0r/libulz
 butchrepo=http://github.com/rofl0r/butch
-librepo=~/cdev/cdev/lib
-butchrepo=~/cdev/cdev/pkg
+librepo=~/libulz
+butchrepo=~/butch
 
 # running make once so the .rcb file gets populated
 make || exit 1
@@ -19,34 +19,41 @@ rm -rf $tempdir_b
 mkdir -p $tempdir_b
 
 # creating temporary Makefile
-cat butch.rcb | rcb2make butch > $tempdir/Makefile
+cat butch.rcb | sed 's@[ \t]\.\./@ @g' | ../rcb/rcb2make.pl butch > $tempdir_b/Makefile
 this="$PWD"
 
 cd $tempdir_b
-git clone "$librepo" lib
+git clone "$librepo" libulz
 git clone "$butchrepo" butch
+for i in butch.c LICENSE README.md ; do
+	mv butch/$i .
+done
+sed -i 's@"../lib/@"libulz/@g' butch.c
+rm -rf butch
+rm -rf libulz/.git
+rm -rf libulz/examples
+rm -rf libulz/tests
+rm -rf libulz/lib
+rm -f libulz/Makefile
+rm -f libulz/.gitignore
 
-for i in `find $tempdir_b/lib -name '*.c'` ; do
+u=0
+uu=0
+for i in `find $tempdir_b/libulz -name '*.c'` ; do
 	f=$(basename "$i")
 	found=0
 	for j in `awk '{ if($1 = "DEP:") print $2}' $this/butch.rcb` ; do
 		if [ $(basename "$j") = "$f" ] ; then
+			u=$(($u + 1))
 			found=1
 			break;
 		fi
 	done
-	[ $found = 0 ] && rm "$i"
+	[ $found = 0 ] && { uu=$(($uu + 1)) ; echo rm "$i"; rm "$i" ; }
 done
+echo "used/unused files from libulz: $u/$uu"
 
-mv $tempdir/Makefile $tempdir_b/butch/
-cat << EOF > build.sh
-#!/bin/sh
-make -C butch
-EOF
-chmod +x build.sh
-rm -rf butch/.git
-rm -rf butch/sha2/tests
-rm -rf lib/.git
 cd $tempdir
 tar cjf butch-$VER.tar.bz2 butch-$VER
 mv butch-$VER.tar.bz2 $me/butch-$VER.tar.bz2
+ls -la $me/butch-$VER.tar.bz2
